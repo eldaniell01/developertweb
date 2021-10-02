5996552aaea051aee21008f1df215ccf720f412c
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-02 00:49:27 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-import "./app.css"
+import "./app.css";
 ReactDOM.render(
+    <div className="login">hola</div>,
     <h1>hola mundo</h1>, document.getElementById("root"));
