8c004947bcc01b5aae965719f5a0791a64cc2712
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-02 00:50:55 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+//todo lo que va dentro de los divs
 import React from "react";
 import  ReactDOM  from "react-dom";
 import "./app.css";
 ReactDOM.render(
-    <div className="login">hola</div>,
+    
     <h1>hola mundo</h1>, document.getElementById("root"));
