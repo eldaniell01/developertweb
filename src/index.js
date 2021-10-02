ca4db0b33b50129f5579b92f885e376a56975b2c
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-02 10:21:52 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ import React from "react";
 import  ReactDOM  from "react-dom";
 import "./app.css";
 ReactDOM.render(
+    <form className="form_log">
+        <center><label htmlFor="title" className="t">HOTEL PRINCY</label></center>
+        <div className="reg">
+            <label className="txt txt2" htmlFor="user">Usuario</label>
+            <input className="txt txt2 txt3" type="text" id="usuario" placeholder="usuario"></input>
+            <label className="txt txt2" htmlFor="pass">Contraseña</label>
+            <input className="txt txt2 txt3" type="password" id="contraseña" placeholder="Contraseña"></input>
+        </div>
+        <div className="buttons">
+            <input className="txt" type="button" id="log" value="Iniciar Sesión"></input>
+            <input className="txt" type="button" id="reg" value="Registrar Usuario"></input>
+        </div>
+    </form>,
     
-    <h1 className="pp">hola mundo</h1>, document.getElementById("root"));
+    document.getElementById("root"));
