1422167042455216eead446be97e498b99242b1c
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-02 15:12:08 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import  ReactDOM  from "react-dom";
 import "./app.css";
 ReactDOM.render(
     
-    <form className="form_log">
+    <><form className="form_log">
         <center><label htmlFor="title" className="t">HOTEL PRINCY</label></center>
         <div className="reg">
             <label className="txt txt2" htmlFor="user">Usuario</label>
@@ -16,6 +16,18 @@ ReactDOM.render(
             <input className="txt but" type="button" id="log" value="Iniciar Sesión"></input>
             <input className="txt but" type="button" id="reg" value="Registrar Usuario"></input>
         </div>
-    </form>,
+    </form>
+    <form className="form_reg">
+        <center><label htmlFor="title" className="t">HOTEL PRINCY</label></center>
+        <label className="txt txt2" htmlFor="user">Nombre</label>
+        <label className="txt txt2" htmlFor="user">Apellido</label>
+        <label className="txt txt2" htmlFor="user">Dirección</label>
+        <label className="txt txt2" htmlFor="user">Teléfono</label>
+        <label className="txt txt2" htmlFor="user">No. DPI</label>
+        <label className="txt txt2" htmlFor="user">Correo Eléctronico </label>
+        <label className="txt txt2" htmlFor="user">Contraseña</label>
+        <label className="txt txt2" htmlFor="user">Repita la contraseña</label>
+        <label className="txt txt2" htmlFor="user">Endiendo los TerminosS</label>
+    </form></>,
     
     document.getElementById("root"));
