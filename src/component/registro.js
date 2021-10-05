c95de082e44497a422b3dbcbfac9afebc4257d54
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-04 22:54:58 -0600

    commit

---
+++ src/component/registro.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+class Registro extends React.Component{
+    constructor(props){
+
+    }
+    render(){
+        retunr( 
+            )
+    }
+}
