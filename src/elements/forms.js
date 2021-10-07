6a526649387cb9516fc4b316bfbcd0162acc555d
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-06 22:46:21 -0600

    commit

---
+++ src/elements/forms.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const colores ={
+    border = "#218DA6",
+    error = "#F20530",
+    complet = "#3FD49B"
+
+}
+
+const Formulario = styled.form`
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 20px;
+`
