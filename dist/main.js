831be99abc2c7308de76cf561f5579d562978879
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-07 22:47:09 -0600

    commit

--- dist/main.js
+++ dist/main.js
@@ -10669,15 +10669,22 @@ var convertCurry = convert.bind(null, react__WEBPACK_IMPORTED_MODULE_2__.createE
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "Formulario": () => (/* binding */ Formulario),
+/* harmony export */   "Ocultar": () => (/* binding */ Ocultar),
+/* harmony export */   "Formulario2": () => (/* binding */ Formulario2),
+/* harmony export */   "Title": () => (/* binding */ Title),
+/* harmony export */   "Textbutton": () => (/* binding */ Textbutton),
 /* harmony export */   "Labell": () => (/* binding */ Labell),
 /* harmony export */   "Inputsgrup": () => (/* binding */ Inputsgrup),
+/* harmony export */   "Buttons": () => (/* binding */ Buttons),
 /* harmony export */   "Textbox": () => (/* binding */ Textbox),
 /* harmony export */   "Leyend": () => (/* binding */ Leyend),
-/* harmony export */   "Iconvalue": () => (/* binding */ Iconvalue)
+/* harmony export */   "Iconvalue": () => (/* binding */ Iconvalue),
+/* harmony export */   "Buttongrup": () => (/* binding */ Buttongrup),
+/* harmony export */   "Iconuser": () => (/* binding */ Iconuser)
 /* harmony export */ });
 /* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
 /* harmony import */ var _fortawesome_react_fontawesome__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @fortawesome/react-fontawesome */ "./node_modules/@fortawesome/react-fontawesome/index.es.js");
-var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5, _templateObject6;
+var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5, _templateObject6, _templateObject7, _templateObject8, _templateObject9, _templateObject10, _templateObject11, _templateObject12, _templateObject13;
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
@@ -10688,12 +10695,19 @@ var colores = {
   error: "#F20530",
   complet: "#3FD49B"
 };
-var Formulario = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].form(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n    display: grid;\n    grid-template-columns: 1fr;\n    gap: 20px;\n    \n"])));
-var Labell = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].label(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n    display: flex;\n    justify-content: center;\n    font-weight: 700;\n    font-size: 1.5em;\n"])));
+var Formulario = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].form(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n    display: grid;\n    grid-template-columns: 1fr;\n    gap: 10px;\n    \n"])));
+var Labell = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].label(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n    display: flex;\n    justify-content: center;\n    font-weight: 700;\n    font-size: 1.1em;\n"])));
 var Inputsgrup = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n    position: relative;\n"])));
-var Textbox = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].input(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n    width: 100%;\n    \n    background: none;\n    border: 3px;\n    height: 45px;\n    text-align: center;\n    padding: 0 40px 0 10 px;\n    line-height: 45px;\n    transition: 0.5s ease all;\n    border: 3px solid transparent;\n    &:focus{\n        border: 3px solid ", ";\n        outline: none;\n    }\n"])), colores.border);
+var Textbox = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].input(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n    width: 100%;\n    background: white;\n    border: 3px;\n    height: 45px;\n    text-align: center;\n    padding: 0 40px 0 40px;\n    line-height: 45px;\n    transition: 0.5s ease all;\n    border: 3px solid transparent;\n    &:focus{\n        border: 3px solid ", ";\n        outline: none;\n    }\n"])), colores.border);
 var Leyend = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].p(_templateObject5 || (_templateObject5 = _taggedTemplateLiteral(["\n    font-family: 'Noto Sans', sans-serif;\n    font-weight: 500;\n    font-size: 12px;\n    margin-bottom: 0%;\n    color: ", ";\n    display: none;\n"])), colores.error);
-var Iconvalue = (0,styled_components__WEBPACK_IMPORTED_MODULE_1__["default"])(_fortawesome_react_fontawesome__WEBPACK_IMPORTED_MODULE_0__.FontAwesomeIcon)(_templateObject6 || (_templateObject6 = _taggedTemplateLiteral(["\n    position: absolute;\n    right: 10px;\n    bottom: 14px;\n    z-index: 100;\n    font-size: 16px;\n"])));
+var Iconvalue = (0,styled_components__WEBPACK_IMPORTED_MODULE_1__["default"])(_fortawesome_react_fontawesome__WEBPACK_IMPORTED_MODULE_0__.FontAwesomeIcon)(_templateObject6 || (_templateObject6 = _taggedTemplateLiteral(["\n    position: absolute;\n    right: 10px;\n    bottom: 14px;\n    z-index: 100;\n    font-size: 16px;\n    display: none;\n"])));
+var Buttongrup = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div(_templateObject7 || (_templateObject7 = _taggedTemplateLiteral(["\n    display: flex;\n    \n    justify-content: center;\n    \n    border: none;\n    margin: 45px;\n"])));
+var Iconuser = (0,styled_components__WEBPACK_IMPORTED_MODULE_1__["default"])(_fortawesome_react_fontawesome__WEBPACK_IMPORTED_MODULE_0__.FontAwesomeIcon)(_templateObject8 || (_templateObject8 = _taggedTemplateLiteral(["\n    position: absolute;\n    left: 10px;\n    bottom: 14px;\n    z-index: 100;\n    font-size: 16px;\n"])));
+var Buttons = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].button(_templateObject9 || (_templateObject9 = _taggedTemplateLiteral(["\n    background: #CDE5F7;\n    transition: 0.4s;\n    margin: 15px;\n    width: 150px;\n    height: 40px;\n    line-height: 40px;\n    border: none;\n    text-align: center;\n    align-items: center;\n    border-radius: 10px;\n    &:hover{\n        background: #82C3F5;\n    }\n"])));
+var Textbutton = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].p(_templateObject10 || (_templateObject10 = _taggedTemplateLiteral(["\n    font-family: 'Noto Sans', sans-serif;\n    font-weight: bold;\n    font-size: 0.9em;\n"])));
+var Title = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].label(_templateObject11 || (_templateObject11 = _taggedTemplateLiteral(["\n    font-family: 'Noto Sans', sans-serif;\n    font-weight: normal;\n    display: flex;\n    justify-content: center;\n    margin: 20px;\n    font-size: 2.4em;\n"])));
+var Formulario2 = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].form(_templateObject12 || (_templateObject12 = _taggedTemplateLiteral(["\n    display: grid;\n    grid-template-columns: 1fr 1fr;\n    gap: 25px;\n"])));
+var Ocultar = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div(_templateObject13 || (_templateObject13 = _taggedTemplateLiteral(["\n    display: none;\n    flex-flow: column;\n    justify-content: center;\n"])));
 
 
 /***/ }),
@@ -10763,19 +10777,112 @@ var Registro = /*#__PURE__*/function (_React$Component) {
   _createClass(Registro, [{
     key: "render",
     value: function render() {
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Formulario, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Formulario, {
+        id: "log"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Title, {
         htmlFor: ""
-      }, "Usuario"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
+      }, "Hotel"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+        htmlFor: ""
+      }, "Email"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
         type: "text",
-        placeholder: "Usuario"
+        placeholder: "Email"
       }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconvalue, {
         icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faCheck
-      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda"))));
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconuser, {
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faUserTie
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+        htmlFor: ""
+      }, "Contrase\xF1a"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
+        type: "text",
+        placeholder: "Contrase\xF1a"
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconvalue, {
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faCheck
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconuser, {
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faKey
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Buttongrup, {
+        id: "bt"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Buttons, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbutton, null, "Iniciar Sesi\xF3n")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Buttons, {
+        onClick: cambiotamaño
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbutton, null, "Crear Usuario")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Ocultar, {
+        id: "sing"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Title, {
+        htmlFor: ""
+      }, "Hotel"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Formulario2, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+        htmlFor: ""
+      }, "Nombre"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
+        type: "text",
+        placeholder: "Nombre"
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconvalue, {
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faCheck
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+        htmlFor: ""
+      }, "Apellido"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
+        type: "text",
+        placeholder: "Apellido"
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconvalue, {
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faCheck
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+        htmlFor: ""
+      }, "Direcci\xF3n"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
+        type: "text",
+        placeholder: "Direcci\xF3n"
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconvalue, {
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faCheck
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+        htmlFor: ""
+      }, "Tel\xE9fono"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
+        type: "text",
+        placeholder: "Tel\xE9fono"
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconvalue, {
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faCheck
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+        htmlFor: ""
+      }, "DPI o Nit"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
+        type: "text",
+        placeholder: "DPI o Nit"
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconvalue, {
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faCheck
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Labell, {
+        htmlFor: ""
+      }, "Correo Electronico"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Inputsgrup, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbox, {
+        type: "text",
+        placeholder: "Correo Electronico"
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Iconvalue, {
+        id: "v",
+        icon: _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faCheck
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Leyend, null, "leyenda"))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Buttongrup, {
+        id: "bt"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Buttons, {
+        onClick: validar
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbutton, null, "Registrar Usuario")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Buttons, {
+        onClick: ocultarregistro
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_elements_forms__WEBPACK_IMPORTED_MODULE_3__.Textbutton, null, "Cancelar"))))));
     }
   }]);
 
   return Registro;
 }(react__WEBPACK_IMPORTED_MODULE_0__.Component);
+
+function cambiotamaño() {
+  var caja = document.getElementsByClassName("login")[0];
+  caja.style.width = 900 + "px";
+  document.getElementById("log").style.display = "none";
+  document.getElementById("bt").style.display = "none";
+  document.getElementById("sing").style.display = "flex";
+  /*document.getElementById("form1").style.opacity ="0";
+  document.getElementById("formr").style.display ="flex";
+  */
+}
+
+function ocultarregistro() {
+  var caja = document.getElementsByClassName("login")[0];
+  caja.style.width = 550 + "px";
+  document.getElementById("log").style.display = "grid";
+  document.getElementById("bt").style.display = "flex";
+  document.getElementById("sing").style.display = "none";
+}
+
+function validar() {}
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Registro);
 
--- src/registro.js
+++ src/registro.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import {render} from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styled from "styled-components";
-import {Formulario, Labell, Inputsgrup, Textbox, Leyend, Iconvalue} from "./elements/forms";
+import {Formulario, Formulario2, Ocultar, Buttons, Title, Labell, Textbutton, Inputsgrup, Textbox, Buttongrup, Leyend, Iconvalue, Iconuser} from "./elements/forms";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faUserTie, faKey } from '@fortawesome/free-solid-svg-icons';
 
 class Registro extends React.Component{
     constructor(props){
@@ -15,20 +15,126 @@ class Registro extends React.Component{
         return( 
             <>
             <main>
-              <Formulario>
-                <Labell htmlFor="">Usuario</Labell>
+              <Formulario id="log">
+                <Title htmlFor="">Hotel</Title>
+                <Labell htmlFor="">Email</Labell>
                 <Inputsgrup>
-                  <Textbox type="text" placeholder="Usuario"></Textbox>
+                  <Textbox type="text" placeholder="Email"></Textbox>
                   <Iconvalue icon={faCheck}></Iconvalue>
+                  <Iconuser icon={faUserTie}></Iconuser>
                 </Inputsgrup>
-                
                 <Leyend>leyenda</Leyend>
+                <Labell htmlFor="">Contraseña</Labell>
+                <Inputsgrup>
+                  <Textbox type="text" placeholder="Contraseña"></Textbox>
+                  <Iconvalue icon={faCheck}></Iconvalue>
+                  <Iconuser icon={faKey}></Iconuser>
+                </Inputsgrup>
+                <Leyend>leyenda</Leyend>
+                
               </Formulario>
+              <Buttongrup id="bt">
+                <Buttons><Textbutton>Iniciar Sesión</Textbutton></Buttons>
+                <Buttons onClick={cambiotamaño}><Textbutton>Crear Usuario</Textbutton></Buttons>
+              </Buttongrup> 
+            </main>
+
+
+            <main>
+              <Ocultar id="sing">
+              <Title htmlFor="">Hotel</Title>
+              <Formulario2>
+              
+                <div>
+                  <Labell htmlFor="">Nombre</Labell>
+                  <Inputsgrup>
+                    <Textbox type="text" placeholder="Nombre"></Textbox>
+                    <Iconvalue icon={faCheck}></Iconvalue>
+                  </Inputsgrup>
+                  <Leyend>leyenda</Leyend>                
+                </div>
+                <div> 
+                  <Labell htmlFor="">Apellido</Labell>
+                  <Inputsgrup>
+                    <Textbox type="text" placeholder="Apellido"></Textbox>
+                    <Iconvalue icon={faCheck}></Iconvalue>
+                  </Inputsgrup>
+                  <Leyend>leyenda</Leyend>
+                </div>
+                
+                <div>
+                  <Labell htmlFor="">Dirección</Labell>
+                  <Inputsgrup>
+                    <Textbox type="text" placeholder="Dirección"></Textbox>
+                    <Iconvalue icon={faCheck}></Iconvalue>
+                  </Inputsgrup>
+                  <Leyend>leyenda</Leyend>
+                </div>
+                
+                <div>
+                  <Labell htmlFor="">Teléfono</Labell>
+                  <Inputsgrup>
+                    <Textbox type="text" placeholder="Teléfono"></Textbox>
+                    <Iconvalue icon={faCheck}></Iconvalue>
+                  </Inputsgrup>
+                  <Leyend>leyenda</Leyend>
+                </div>
+                
+                <div>
+                  <Labell htmlFor="">DPI o Nit</Labell>
+                  <Inputsgrup>
+                    <Textbox type="text" placeholder="DPI o Nit"></Textbox>
+                    <Iconvalue icon={faCheck}></Iconvalue>
+                  </Inputsgrup>
+                  <Leyend>leyenda</Leyend>
+                </div>
+                
+                <div>
+                  <Labell htmlFor="">Correo Electronico</Labell>
+                  <Inputsgrup>
+                    <Textbox type="text" placeholder="Correo Electronico"></Textbox>
+                    <Iconvalue id="v" icon={faCheck}></Iconvalue>
+                  </Inputsgrup>
+                  <Leyend>leyenda</Leyend>
+                </div>
+                
+              </Formulario2>
+              
+              <Buttongrup id="bt">
+                <Buttons onClick={validar}><Textbutton>Registrar Usuario</Textbutton></Buttons>
+                <Buttons onClick={ocultarregistro}><Textbutton>Cancelar</Textbutton></Buttons>
+              </Buttongrup> 
+              </Ocultar>
+              
             </main>
             </>  
         );
     }
 }
 
+function cambiotamaño(){
+        
+  var caja = document.getElementsByClassName("login")[0];
+  caja.style.width= 900 +"px";
+
+  document.getElementById("log").style.display ="none";
+  document.getElementById("bt").style.display ="none";
+  document.getElementById("sing").style.display = "flex";
+  /*document.getElementById("form1").style.opacity ="0";
+  document.getElementById("formr").style.display ="flex";
+*/
+}
+
+function ocultarregistro(){
+  var caja = document.getElementsByClassName("login")[0];
+  caja.style.width= 550 +"px";
+  document.getElementById("log").style.display ="grid";
+  document.getElementById("bt").style.display ="flex";
+  document.getElementById("sing").style.display = "none";
+}
+
+function validar(){
+  
+}
 
 export default Registro;
