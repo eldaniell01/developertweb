fc0566011694d4a77bc027a6a4e8912eab247e1e
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-16 13:23:40 -0600

    commit

--- api/bd/executes.php
+++ api/bd/executes.php
@@ -21,5 +21,45 @@
             }
             return $vector;
         }
+        public function getmultiusers($id){
+            $vector =array();
+            $conexion = new conexion();
+            $bd = $conexion->getconexion();
+            $sql = "select *from usuario where id=:id";
+            $consult = $bd->prepare($sql);
+            $consult->bindParam(':id', $id);
+            $consult->execute();
+            while($fila=$consult->fetch()){
+                $vector[] = array(
+                    "id" => $fila['id'],
+                    "nombre"=> $fila['nombre'],
+                    "apellido"=>$fila['apellido'],
+                    "dirección"=>$fila['direccion'],
+                    "telefono"=>$fila['telefono'],
+                    "dpi"=>$fila['dpi'],
+                    "correo"=>$fila['correo'],
+                    "pass"=>$fila['pass'],
+                );
+            }
+            return $vector[0];
+        }
+
+        public function addProducto($nombre, $apellido, $direccion, $telefono, $dpi, $correo, $pass){
+  
+            $conexion = new Conexion();
+            $bd = $conexion->getconexion();
+            $sql = "INSERT INTO producto (nombre, apellido, dirección, telefono, dpi, correo, pass) VALUES (:nombre, :apellido, :dirección, :telefono, :dpi, :correo, :pass)";
+            $consult = $bd->prepare($sql);
+            $consult->bindParam(':nombre', $nombre);
+            $consult->bindParam(':apellido', $apellido);
+            $consult->bindParam(':dirección', $apellido);
+            $consult->bindParam(':telefono', $apellido);
+            $consult->bindParam(':dpi', $apellido);
+            $consult->bindParam(':correo', $apellido);
+            $consult->bindParam(':pass', $apellido);
+            $consult->execute();
+          
+            return '{"msg":" usuario registrado"}';
+          }
     }
 ?>
--- api/index.php
+++ api/index.php
@@ -3,5 +3,19 @@
     require_once('./bd/cors.php');
     require_once('./bd/executes.php');
     $method =$_SERVER['REQUEST_METHOD'];
-    
+    if($method =="GET"){
+        if(!empty($_get['id'])){
+            $id=$_GET['id'];
+            $api = new executes();
+            $obj =$api->getmultiusers($id);
+            $json = json_encode($obj);
+            echo $json;
+        }else{
+            $vector = array();
+            $api = new executes();
+            $vector = $api->getusers();
+            $json = json_encode($vector);
+            echo $json;
+        }
+    }
 ?>
