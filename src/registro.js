59b0be0df34735ed3d08a17b68739cb3d05218d0
eldaniell01 <amontepequem@miumg.edu.gt>
2021-10-16 22:17:47 -0600

    commit

--- src/registro.js
+++ src/registro.js
@@ -27,8 +27,135 @@ function app(){
       const res = await axios.post('http://localhost:3000/api/', obj);
       console.log(res);
     }
+    return( 
+      <>
+      <main>
+        <Formulario id="log">
+          <Title htmlFor="">Hotel</Title>
+          <Labell htmlFor="">Email</Labell>
+          <Inputsgrup>
+            <Textbox type="text" placeholder="Email" id="textname"></Textbox>
+            <Iconvalue icon={faCheck}></Iconvalue>
+            <Iconuser icon={faUserTie}></Iconuser>
+          </Inputsgrup>
+          <Leyend>leyenda</Leyend>
+          <Labell htmlFor="">Contraseña</Labell>
+          <Inputsgrup>
+            <Textbox type="password" placeholder="******" id="pass"></Textbox>
+            <Iconvalue icon={faCheck}></Iconvalue>
+            <Iconuser icon={faKey} onClick={showpass}></Iconuser>
+          </Inputsgrup>
+          <Leyend id="leyenl">leyenda</Leyend>
+          <div>
+          <Labell htmlFor="" id="ter">
+            <Confirm type="checkbox" name="terminos" id="terminos" onClick={showpass}>
+            </Confirm>Mostrartrar contraseña
+          </Labell> 
+        </div>
+        </Formulario>
+        
+        <Buttongrup id="bt">
+          <Buttons onClick={validarlog}><Textbutton>Iniciar Sesión</Textbutton></Buttons>
+          <Buttons onClick={cambiotamaño}><Textbutton>Crear Usuario</Textbutton></Buttons>
+        </Buttongrup> 
+      </main>
+
+
+      <main>
+        <Ocultar id="sing">
+        <Title htmlFor="">Hotel</Title>
+        <Formulario2>
+        
+          <div>
+            <Labell htmlFor="">Nombre</Labell>
+            <Inputsgrup>
+              <Textbox type="text" placeholder="Nombre"></Textbox>
+              <Iconvalue icon={faCheck}></Iconvalue>
+            </Inputsgrup>
+            <Leyend>leyenda</Leyend>                
+          </div>
+          <div> 
+            <Labell htmlFor="">Apellido</Labell>
+            <Inputsgrup>
+              <Textbox type="text" placeholder="Apellido"></Textbox>
+              <Iconvalue icon={faCheck}></Iconvalue>
+            </Inputsgrup>
+            <Leyend>leyenda</Leyend>
+          </div>
+          
+          <div>
+            <Labell htmlFor="">Dirección</Labell>
+            <Inputsgrup>
+              <Textbox type="text" placeholder="Dirección"></Textbox>
+              <Iconvalue icon={faCheck}></Iconvalue>
+            </Inputsgrup>
+            <Leyend>leyenda</Leyend>
+          </div>
+          
+          <div>
+            <Labell htmlFor="">Teléfono</Labell>
+            <Inputsgrup>
+              <Textbox type="text" placeholder="Teléfono"></Textbox>
+              <Iconvalue icon={faCheck}></Iconvalue>
+            </Inputsgrup>
+            <Leyend>leyenda</Leyend>
+          </div>
+          
+          <div>
+            <Labell htmlFor="">DPI o Nit</Labell>
+            <Inputsgrup>
+              <Textbox type="text" placeholder="DPI o Nit"></Textbox>
+              <Iconvalue icon={faCheck}></Iconvalue>
+            </Inputsgrup>
+            <Leyend>leyenda</Leyend>
+          </div>
+          
+          <div>
+            <Labell htmlFor="">Correo Electronico</Labell>
+            <Inputsgrup>
+              <Textbox type="text" placeholder="Correo Electronico"></Textbox>
+              <Iconvalue id="ch" icon={faCheck}></Iconvalue>
+            </Inputsgrup>
+            <Leyend>leyenda</Leyend>
+          </div>
+          <div>
+            <Labell htmlFor="">Contraseña</Labell>
+            <Inputsgrup>
+              <Textbox type="password" placeholder="*********"  id="pass2"></Textbox>
+              <Iconvalue id="v" icon={faCheck}></Iconvalue>
+              <Iconuser icon={faKey} onClick={showpass}></Iconuser>
+            </Inputsgrup>
+            <Leyend>leyenda</Leyend>
+          </div>
+          <div>
+            <Labell htmlFor="">Repetir Contraseña</Labell>
+            <Inputsgrup>
+              <Textbox type="password" placeholder="*********" id="pass3"></Textbox>
+              <Iconvalue id="v" icon={faCheck}></Iconvalue>
+              <Iconuser icon={faKey} onClick={showpass} ></Iconuser>
+            </Inputsgrup>
+            <Leyend>leyenda</Leyend>
+          </div>
+        </Formulario2>
+        <div>
+          <Labell htmlFor="" id="ter">
+            <Confirm type="checkbox" name="terminos" id="terminos">
+            </Confirm>Aceptar Terminos y Condiciones
+          </Labell> 
+        </div>
+        
+        <Buttongrup id="bt">
+          <Buttons ><Textbutton>Registrar Usuario</Textbutton></Buttons>
+          <Buttons onClick={ocultarregistro}><Textbutton>Cancelar</Textbutton></Buttons>
+        </Buttongrup> 
+        </Ocultar>
+        
+      </main>
+      </>  
+  );
 
 }
+/*
 class Registro extends React.Component{
     constructor(props){
         super(props);
@@ -163,7 +290,7 @@ class Registro extends React.Component{
             </>  
         );
     }
-}
+}*/
 
 function cambiotamaño(){
         
